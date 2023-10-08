94c7f83da28f10d7700e5de993fc0699b3c78d41
Nekit <110564482+Nekitmm5cs@users.noreply.github.com>
2023-10-08 23:46:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def get_memory_info():
 
 def get_process_info():
     process_info = {}
-    for proc in psutil.process_iter(['pid', 'name', 'nice', 'cpu_percent', 'status']):
+    for proc in psutil.process_iter(['pid', 'name', 'nice', 'status']):
         info = proc.info
         pid = info.get('pid')
         if pid is not None:
@@ -27,11 +27,34 @@ def get_process_info():
     return process_info
 
 def hat_table_show():
-    print("{:_^10}{:_^35}{:_^10}{:_^12}{:_^14}".format("_", "_", "_", "_", "_", "_"))
-    print("|{:^5}|{:^35}|{:^10}|{:^12}|{:^13}|".format('№', 'Name', 'Priority', 'PID', 'Status'))
-    print("|{:_^5}|{:_^35}|{:_^10}|{:_^12}|{:_^13}|".format("_", "_", "_", "_", "_", "_"))
+    print("{:_^10}{:_^35}{:_^15}{:_^12}{:_^14}".format("_", "_", "_", "_", "_"))
+    print("|{:5}|{:^35}|{:^15}|{:^12}|{:^13}|".format('№', 'Name', 'PID', 'Status', 'Priority'))
+    print("|{:_^5}|{:_^35}|{:_^15}|{:_^12}|{:_^13}|".format("_", "_", "_", "_", "_"))
 
-def process_table_show:
+    def iter_process_info():
+        number_column = 0
+        for list_iter in psutil.process_iter():
+            number_column += 1
+            pid = list_iter.as_dict(['pid'])
+            name = list_iter.as_dict(['name'])
+            priority = list_iter.as_dict(['nice'])
+            status = list_iter.as_dict(['status'])
+        print (
+            "║{:^5}│{:<35}│{:^15}│{:^13}║".format(pid['pid'], name['name'], priority['nice'], status['status']))
+
+    iter_process_info()
+
+def main():
+    get_process_info()
+    get_cpu_info()
+    get_memory_info()
+    hat_table_show()
+
+main()
+
+print(get_process_info())
+
+
 
 
 
@@ -43,4 +66,4 @@ def process_table_show:
 
 # print(get_memory_info())
 # print(get_cpu_info())
-print(get_process_info())
+# print(get_process_info())
