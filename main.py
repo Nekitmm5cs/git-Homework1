b33bf5744265583777a5be7c604267d5993f5ae4
Nekit <110564482+Nekitmm5cs@users.noreply.github.com>
2023-10-08 20:49:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -19,15 +19,28 @@ def get_memory_info():
 
 def get_process_info():
     process_info = {}
-    for proc in psutil.process_iter(['pid', 'name']):
+    for proc in psutil.process_iter(['pid', 'name', 'nice', 'cpu_percent', 'status']):
         info = proc.info
         pid = info.get('pid')
         if pid is not None:
-            process_info[pid] = {"name": info['name']}
+            process_info[pid] = {"name": info['name'], "priority": info["nice"], 'status': info['status']}
     return process_info
 
+def hat_table_show():
+    print("{:_^10}{:_^35}{:_^10}{:_^12}{:_^14}".format("_", "_", "_", "_", "_", "_"))
+    print("|{:^5}|{:^35}|{:^10}|{:^12}|{:^13}|".format('№', 'Name', 'Priority', 'PID', 'Status'))
+    print("|{:_^5}|{:_^35}|{:_^10}|{:_^12}|{:_^13}|".format("_", "_", "_", "_", "_", "_"))
+
+def process_table_show:
 
 
-print(get_memory_info())
-print(get_cpu_info())
+
+
+
+
+# print(process_table_show())
+
+
+# print(get_memory_info())
+# print(get_cpu_info())
 print(get_process_info())
